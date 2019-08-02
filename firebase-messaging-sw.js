3befab46a66972fa887b182ca09852f8e3a97ce4
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2019-08-03 01:38:14 +0300

    firebase webpush

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  messagingSenderId: "821062676642"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+//fdsafd
+
+self.addEventListener('install', function() {
+  console.log('install firebase messaging sw');
+});
+
+self.addEventListener('activate', function() {
+  console.log('activate fireabase messaging sw');
+  registration.showNotification("activate", {
+    title: 'activate',
+    body: 'worker activate'
+  });
+});
+
+messaging.setBackgroundMessageHandler(function() {
+  console.log(arguments);
+})
---
+++ init-firebase.js
@@ -0,0 +1,39 @@
+navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+  .then(function(reg) {
+    console.log('[Companion]', 'Service worker registered!');
+  });
+
+const firebaseConfig = {
+  messagingSenderId: "821062676642"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+window.messaging = firebase.messaging();
+
+function subscribe() {
+    // запрашиваем разрешение на получение уведомлений
+    messaging.requestPermission()
+        .then(function () {
+            // получаем ID устройства
+            messaging.getToken()
+                .then(function (currentToken) {
+                    console.log(currentToken);
+
+                    if (currentToken) {
+                        // sendTokenToServer(currentToken);
+                    } else {
+                        console.warn('Не удалось получить токен.');
+                        // setTokenSentToServer(false);
+                    }
+                })
+                .catch(function (err) {
+                    console.warn('При получении токена произошла ошибка.', err);
+                    // setTokenSentToServer(false);
+                });
+    })
+    .catch(function (err) {
+        console.warn('Не удалось получить разрешение на показ уведомлений.', err);
+    });
+}
