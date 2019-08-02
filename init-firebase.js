c89f74650a590ae613a4830eb7c90fa6e1577494
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2019-08-03 01:53:11 +0300

    Убрал дублирующую активацию sw

--- init-firebase.js
+++ init-firebase.js
@@ -1,8 +1,3 @@
-navigator.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/' })
-  .then(function(reg) {
-    console.log('[Companion]', 'Service worker registered!');
-  });
-
 const firebaseConfig = {
   messagingSenderId: "821062676642"
 };
