5176a26478696503511c033b539ec61d8faab159
cortc <konstantin.gorozhankin@gmail.com>
2025-02-04 15:13:20 +0500

    Initial commit for videojs

---
+++ script.js
@@ -0,0 +1,70 @@
+window.App = {
+  videos: [],
+  play () {
+    this.videos.forEach(v => v.play());
+  },
+  seek (value) {
+    console.log(value);
+    const time = Number(value/1000);
+    this.videos.forEach(v => {
+      v.currentTime(time);
+    });
+  }
+}
+
+function init (src) {
+  const container = document.getElementById("container");
+  const video = document.createElement("video");
+  video.className = "b-container__video video-js vjs-default-skin"
+
+  video.addEventListener("durationchange", function (event) {
+    console.log("durationchange", event.target.duration)
+  })
+
+  video.addEventListener("timeupdate", function () {
+    const currentTimes = App.videos.map(p => p.currentTime());
+    const delta = Math.abs(currentTimes[0] - currentTimes[1]);
+
+    if (delta > 0.005) {
+      console.log(`%c ${delta}`, 'background: red; color: white; padding: 2px;');
+      const isReady = App.videos.map(p => p.readyState()).every(state => state === 4)
+
+      if (isReady) {
+        App.videos.forEach(p => {
+          p.currentTime(currentTimes[0]);
+        });
+      }
+    }
+    else {
+      console.log(delta);
+    }
+  });
+
+  video.addEventListener("waiting", function () {
+    console.log("waiting");
+  })
+
+  video.addEventListener("canplay", function () {
+    console.log("canplay")
+    const isReady = App.videos.map(p => p.readyState()).every(p => p === 4);
+    if (isReady) {
+      playButton.removeAttribute("disabled");
+    }
+  })
+
+  container.appendChild(video);
+
+  const player = videojs(video, {
+    controls: false,
+    sources: [{
+      src,
+      type: "application/x-mpegurl"
+    }]
+  });
+
+  App.videos.push(player);
+}
+
+init("https://flussonic-cdn.vod4.mediastore.app/cas/mcentral/streaming/2025/01/24/3388469/20250124-1526-1466-8002-4b13849121fd/video-1737732378660.smil/index.m3u8?token=2202505160000d6f8f687caa3b0fa50403cf3435a8aee0ddec891382e144176a184179b2be6a7");
+init("https://flussonic-cdn.vod4.mediastore.app/cas/mcentral/streaming/2025/01/24/3388468/20250124-1523-2123-2117-3eaad944a1eb/video-1737732202728.smil/index.m3u8?token=22025051600008661b1ee789b03a996d2179de039fe7b9d1fab049d3f582b1d30ff2a66b54a0f");
+
