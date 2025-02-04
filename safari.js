d36f524d39dd26df680734ae14aa7719c4b84120
cortc <konstantin.gorozhankin@gmail.com>
2025-02-04 17:44:25 +0500

    Add page for safari

--- safari.js
+++ safari.js
@@ -1,19 +1,19 @@
 window.App = {
-  players: [],
+  videos: [],
   play () {
-    this.players.forEach(p => p.play());
+    this.videos.forEach(v => v.play());
   },
   pause () {
-    this.players.forEach(p => p.pause());
+    this.videos.forEach(v => v.pause());
   },
   muted () {
-    this.players.forEach(p => p.muted(false))
+    this.videos.forEach(v => v.muted = false)
   },
   seek (value) {
     console.log(value);
     const time = Number(value/1000);
-    this.players.forEach(p => {
-      p.currentTime(time);
+    this.videos.forEach(v => {
+      v.currentTime = time;
     });
   }
 }
@@ -22,22 +22,23 @@ function init (src) {
   const container = document.getElementById("container");
   const video = document.createElement("video");
   video.className = "b-container__video video-js vjs-default-skin"
+  video.muted = true;
 
   video.addEventListener("durationchange", function (event) {
     console.log("durationchange", event.target.duration)
   })
 
   video.addEventListener("timeupdate", function () {
-    const currentTimes = App.players.map(p => p.currentTime());
+    const currentTimes = App.videos.map(v => v.currentTime);
     const delta = Math.abs(currentTimes[0] - currentTimes[1]);
 
-    if (delta > 0.005) {
+    if (delta > 0.03) {
       console.log(`%c${delta}`, 'background: red; color: white; padding: 2px 5px;');
-      const isReady = App.players.map(p => p.readyState()).every(state => state === 4)
+      const isReady = App.videos.map(v => v.readyState).every(state => state === 4)
 
       if (isReady) {
-        App.players.forEach(p => {
-          p.currentTime(currentTimes[0]);
+        App.videos.forEach(p => {
+          p.currentTime = currentTimes[0];
         });
       }
     }
@@ -52,24 +53,27 @@ function init (src) {
 
   video.addEventListener("canplay", function () {
     console.log("canplay")
-    const isReady = App.players.map(p => p.readyState()).every(p => p === 4);
+    const isReady = App.videos.map(v => v.readyState).every(v => v === 4);
     if (isReady) {
       playButton.removeAttribute("disabled");
     }
-  })
+  });
+
+  video.addEventListener("canplaythrough", function (event) {
+    const isReady = App.videos.map(v => v.readyState).every(v => v === 4);
+    if (isReady) {
+      playButton.removeAttribute("disabled");
+    }
+  });
+
+  const source = document.createElement("source");
+  source.src = src;
+  source.type = "application/x-mpegurl";
+  video.appendChild(source);
 
   container.appendChild(video);
 
-  const player = videojs(video, {
-    controls: false,
-    muted: true,
-    sources: [{
-      src,
-      type: "application/x-mpegurl"
-    }]
-  });
-
-  App.players.push(player);
+  App.videos.push(video);
 }
 
 init("https://flussonic-cdn.vod4.mediastore.app/cas/mcentral/streaming/2025/01/24/3388469/20250124-1526-1466-8002-4b13849121fd/video-1737732378660.smil/index.m3u8?token=2202505160000d6f8f687caa3b0fa50403cf3435a8aee0ddec891382e144176a184179b2be6a7");
